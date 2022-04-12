1cf0e7c71381924f49c50c13135ce64695e0bf7f
Aishwarya P <93313435+Aishwaryapatil1@users.noreply.github.com>
2022-04-12 19:03:30 +0530

    Question1 completed

---
+++ Day2-React-Introduction/question1/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const structure = [
+    {
+      heading: "Mobile Operating System",
+      data: [
+        { name: "Android", style: "disc" },
+        { name: "Blackberry", style: "disc" },
+        { name: "iPhone", style: "disc" },
+        { name: "windows phone", style: "disc" },
+      ],
+    },
+    {
+      heading: "Mobile Manufacturers",
+      data: [
+        { name: "samsung", style: "square" },
+        { name: "HTC", style: "square" },
+        { name: "micromax", style: "disc" },
+        { name: "Apple", style: "circle" },
+      ],
+    },
+  ];
+
+  return (
+    <div className="App">
+      {structure.map((e) => {
+        return (
+          <Heading
+            heading={e.heading}
+            data={e.data.map((el) => {
+              return <li className={el.style}> {el.name}</li>;
+            })}
+          />
+        );
+      })}
+    </div>
+  );
+}
+function Heading({ heading, data }) {
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <ul>{data}</ul>
+    </div>
+  );
+}
+
+export default App;
