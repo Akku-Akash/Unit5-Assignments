bdfb0fdcf6aa8a065ed3b97ab4fb8f8e7213e613
Aishwarya P <93313435+Aishwaryapatil1@users.noreply.github.com>
2022-04-12 19:11:06 +0530

    question 2 done

--- Day2-React-Introduction/question2/src/App.js
+++ Day2-React-Introduction/question2/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const links= ["services", "project", "About"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h3>LOGOBAKERY</h3>
+       <div className="middle">
+          {
+            links.map((e)=>{
+              return <Links link={e}/>
+            })
+          }
+       </div>
+       <button className="btn">contacts</button>
+      
     </div>
   );
+}
+function Links(props){
+  return <p className="links">{props.link}</p>
 }
 
 export default App;
